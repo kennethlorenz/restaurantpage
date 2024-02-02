eccf4798d82a542c6fd719deb2213806e3cf05ce
kz <kennethlorenzg@gmail.com>
2024-02-01 23:16:54 -0500

    Removed hero content

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("test .js");
+(()=>{"use strict";document.querySelector("#content").appendChild(function(){const e=document.createElement("div"),t=document.createElement("h1"),n=document.createElement("p"),i=document.createElement("button");return e.classList.add("hero"),t.innerHTML="Delicious pizza for your headtop!",n.innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellateaque soluta tempora ipsa ipsum aliquid.",i.innerHTML="Order Now",e.append(t,n,i),e}())})();
