a76c087078e386294bc7da634608643141d6aae9
kz <kennethlorenzg@gmail.com>
2024-02-10 22:25:19 -0500

    add updateContent function to update main content and nav

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import home from "./home";
+import menu from "./menu";
+import contact from "./contact";
 
 const content = document.querySelector("#content");
+const homeNavBtn = document.getElementById("home");
+const menuNavBtn = document.getElementById("menu");
+const contactNavBtn = document.getElementById("contact");
 
 content.appendChild(home());
+
+homeNavBtn.addEventListener("click", () => {
+  udpateContent(home(), homeNavBtn);
+});
+
+menuNavBtn.addEventListener("click", () => {
+  udpateContent(menu(), menuNavBtn);
+});
+contactNavBtn.addEventListener("click", () => {
+  udpateContent(contact(), contactNavBtn);
+});
+
+function udpateContent(page, nav) {
+  content.replaceChildren(page);
+  setActiveNav(nav);
+}
+
+function setActiveNav(selectedNav) {
+  const navButtons = document.querySelectorAll(".nav");
+  navButtons.forEach((nav) => {
+    if (nav.id !== selectedNav.id) {
+      nav.classList.remove("bold");
+    }
+  });
+  selectedNav.classList.add("bold");
+}
