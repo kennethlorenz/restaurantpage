c57d99bcbb1709c3fb6c2ca86789f1c9595206c1
kz <kennethlorenzg@gmail.com>
2024-02-15 21:38:00 -0500

    Import pizza photos, update createmenu item

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,11 @@
+import carne from "./photos/carne.png";
+import colorato from "./photos/colorato.png";
+import crema from "./photos/crema.png";
+import disgustoso from "./photos/disgustoso.png";
+import gamberi from "./photos/gamberi.png";
+import pepe from "./photos/pepe.png";
+import pomodoro from "./photos/pomodoro.png";
+import salsiccia from "./photos/salsiccia.png";
 export default function menu() {
   const content = document.createElement("div");
   content.classList.add("menu");
@@ -5,36 +13,42 @@ export default function menu() {
   content.append(
     createMenuItem(
       "Carne",
+      carne,
       "Tomato sauce, Mozarella, Tomato, Homemade sausage, Garlic, Basil"
     )
   );
   content.append(
     createMenuItem(
       "Colorato",
+      colorato,
       "Tomato sauce, Mozarella, Onion, Pepper, Champignons, Basil"
     )
   );
   content.append(
     createMenuItem(
       "Gamberi",
+      gamberi,
       "Tomato sauce, Mozarella, Shrimps, Feta cheese, Olives, Basil"
     )
   );
   content.append(
     createMenuItem(
       "Pepe",
+      pepe,
       "Tomato sauce, Mozarella, Chilli flakes, Olives, Basil"
     )
   );
   content.append(
     createMenuItem(
       "Disgustoso",
+      disgustoso,
       "Tomato sauce, Bacon, Pineapple, Olives, Basil"
     )
   );
   content.append(
     createMenuItem(
       "Salsiccia",
+      salsiccia,
       "Tomato sauce, Mozarella, Tomato, Homemade sausage, Garlic, Basil"
     )
   );
@@ -54,7 +68,7 @@ export default function menu() {
   return content;
 }
 
-function createMenuItem(pizzaName, description) {
+function createMenuItem(pizzaName, img, description) {
   const card = document.createElement("div");
   card.classList.add("menu-item");
 
